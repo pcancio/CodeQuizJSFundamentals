f53372c08aa74d9bb7f1a7c550c7b9b02738c48c
pcancio <pcancio@gmail.com>
2021-11-29 21:52:55 -0500

    added variables to match html ids

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,11 @@ var Questions = [{
         correct: ""
     },
 ]
+var questionID = 0;
+var score = 0;
+var quizContainerEl = document.querySelector("#quiz-container");
+var scoreEl = document.querySelector("#totalScore");
+var timerEl = document.querySelector("#timer");
+var main = document.querySelector("#main");
+var timeLeft = 60;
+var HighScore = [];
