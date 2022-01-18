776c0974a9aac37ada7385b44add209c690ce20d
pcancio <pcancio@gmail.com>
2022-01-17 22:48:55 -0500

    buildQuiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,26 @@ function startQuiz() {
 function sendMessage() {
     alert("Game over! Enter your score.")
 }
+var card = document.getElementById('welcomeCard')
+
+function buildQuiz(quizQ) {
+    card.innerHTML = `
+    <div> ${ quizQ.question} </div> 
+        <button class = "answerBtn"> ${ quizQ.choice1 } </button> 
+        <button class = "answerBtn" > ${ quizQ.choice2 } </button> 
+        <button class = "answerBtn" > ${ quizQ.choice3 } </button> 
+        <button class = "answerBtn" > ${ quizQ.choice4 } </button>`
+}
+var userSelection = document.getElementsByClassName('answerBtn')
+for (var i = 0; i < userSelection.length; i++) {
+    userSelection[i].addEventListener('click', function(event) {
+        if (event.target.innerText === quizQ.answer) {} else {
+            secondsLeft -= 10
+        }
+        questionIndex++
+        if (questionIndex === quizQuestions.length) {
+            endGame()
+            timeEl = secondsLeft
+        } else { buildQuiz(quizQuestions[questionIndex]) }
+    })
+}
