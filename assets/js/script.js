2a368ce4e9b14e4b0fccdcf184f1c52e6a27e2e2
pcancio <pcancio@gmail.com>
2022-02-02 15:14:57 -0500

    createElement function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+var headerEl = document.getElementById("top");
+var contentId = document.getElementById("content");
+
+function createElement(element, type, value, text) {
+    var tmp = document.createElement(element);
+    tmp.setAttribute(type, value);
+    tmp.textContent = text;
+    return tmp;
+};
+
+
+
+
+
+
 var timeEl = document.querySelector(".time");
 var secondsLeft = 75;
 var questionIndex = 0;
@@ -61,44 +76,44 @@ function endGame() {
 }
 
 var quizQuestions = [{
-        question: "",
-        choice1: "",
-        choice2: "",
-        choice3: "",
-        choice4: "",
-        answer: ""
+        question: "Inside which HTML element do we put the JavaScript?",
+        choice1: "<script>",
+        choice2: "<js>",
+        choice3: "<scripting>",
+        choice4: "<javascript>",
+        answer: "<script>"
     },
     {
-        question: "",
-        choice1: "",
-        choice2: "",
-        choice3: "",
-        choice4: "",
-        answer: ""
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        choice1: "<script href='xxx.js'>",
+        choice2: "<script ='xxx.js'>",
+        choice3: "<script src='xxx.js'>",
+        choice4: "<javascript ='xxx.js'>",
+        answer: "<script src='xxx.js'>"
     },
     {
-        question: "",
-        choice1: "",
-        choice2: "",
-        choice3: "",
-        choice4: "",
-        answer: ""
+        question: "How do you write 'Hello World' in an alert box?",
+        choice1: "msgBox('Hello World')",
+        choice2: "alert('Hello World');",
+        choice3: "alertBox('Hello World');",
+        choice4: "msg('Hello World');",
+        answer: "alert('Hello World');"
     },
     {
-        question: "",
-        choice1: "",
-        choice2: "",
-        choice3: "",
-        choice4: "",
-        answer: ""
+        question: "How to write an IF statement in JavaScript?",
+        choice1: "if i = 5",
+        choice2: "if (i==5)",
+        choice3: "if i = 5 then",
+        choice4: "if i == 5 then",
+        answer: "if (i==5)"
     },
     {
-        question: "",
-        choice1: "",
-        choice2: "",
-        choice3: "",
-        choice4: "",
-        answer: ""
+        question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+        choice1: "if (i !=5)",
+        choice2: "if i=!5 then",
+        choice3: "if (i) !=5",
+        choice4: "if i <> 5",
+        answer: "if (i != 5)"
     },
 ]
 
