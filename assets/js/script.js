50b07bae4cdd687361940593776d6d1241e171bc
pcancio <pcancio@gmail.com>
2022-02-21 00:27:44 -0500

    reformat question index

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ const answer3 = document.getElementById("answer3");
 const answer4 = document.getElementById("answer4");
 const answers = document.getElementById("answers");
 const questionText = document.getElementById("questionText");
-const gameContainer = document.getElementById("game-div");
+const quizContainer = document.getElementById("quiz-div");
 const timer = document.querySelector("#time");
 const messageDiv = document.querySelector("#message");
 
@@ -23,157 +23,205 @@ startButton.addEventListener("click", startQuiz);
 // start timer 
 function startTime() {
     let timerInterval = setInterval(() => {
-      time--;
-      timer.textContent = `Timer: ${time}`;
-  
-      if (time === 0) {
-        clearInterval(timerInterval);
-        alert("time's up!");
-        endGame();
-      } else if (questionIndex === questions.length) {
-        clearInterval(timerInterval);
-      }
+        time--;
+        timer.textContent = `Timer: ${time}`;
+
+        if (time === 0) {
+            clearInterval(timerInterval);
+            alert("time's up!");
+            endQuiz();
+        } else if (questionIndex === questions.length) {
+            clearInterval(timerInterval);
+        }
     }, 1000);
     return score;
-  }
-  
+}
+
 // start quiz
 function startQuiz() {
     randQuestion = questions.sort(() => Math.random() - 0.5);
     questionIndex = 0;
     showNextQuestion();
-  }
-  // show next
+}
+// show next
 function showNextQuestion() {
     showQuestion(randQuestion[questionIndex]);
-  }
-  
-  // get next 
-  function showQuestion() {
+}
+
+// get next 
+function showQuestion() {
     answer1.hidden = false;
     answer2.hidden = false;
     answer3.hidden = false;
     answer4.hidden = false;
-  
+
     startButton.classList.add("hide");
     if (questionIndex === questions.length) {
-      endQuiz();
+        endQuiz();
     } else {
-      questionText.innerText = questions[questionIndex].question;
-      answer1.textContent = questions[questionIndex]["choices"][0];
-      answer2.textContent = questions[questionIndex]["choices"][1];
-      answer3.textContent = questions[questionIndex]["choices"][2];
-      answer4.textContent = questions[questionIndex]["choices"][3];
+        questionText.innerText = questions[questionIndex].question;
+        answer1.textContent = questions[questionIndex]["choices"][0];
+        answer2.textContent = questions[questionIndex]["choices"][1];
+        answer3.textContent = questions[questionIndex]["choices"][2];
+        answer4.textContent = questions[questionIndex]["choices"][3];
     }
-  }
-  // select answer
+}
+// select answer
 function selectAnswer(answer) {
     if (
-      questions[questionIndex].answer === questions[questionIndex].choices[answer]
+        questions[questionIndex].answer === questions[questionIndex].choices[answer]
     ) {
-      messageDiv.textContent = "Correct!";
-      score += 20;
+        messageDiv.textContent = "Correct!";
+        score += 20;
     } else {
-      messageDiv.textContent = "Wrong!";
-      score -= 20;
-      time -= 10;
+        messageDiv.textContent = "Wrong!";
+        score -= 20;
+        time -= 10;
     }
     questionIndex++;
     showNextQuestion();
-  }
-  
-  // functions to choose each button for selectAnswer
-  function choice0() {
+}
+
+// functions to choose each button for selectAnswer
+function choice0() {
     selectAnswer(0);
-  }
-  function choice1() {
+}
+
+function choice1() {
     selectAnswer(1);
-  }
-  function choice2() {
+}
+
+function choice2() {
     selectAnswer(2);
-  }
-  function choice3() {
+}
+
+function choice3() {
     selectAnswer(3);
-  }
-  
-  // GAMEOVERRRR RIP QUIZ
+}
+
+// GAMEOVERRRR RIP QUIZ
 function endQuiz() {
     let scoreTag = document.createElement("h1");
     let inputTag = document.createElement("input");
     let submitButton = document.createElement("button");
-  
+
     score += 20;
-  
+
     messageDiv.classList.add("hide");
     questionText.textContent = "Game Over!";
-  
+
     answer1.remove();
     answer2.remove();
     answer3.remove();
     answer4.remove();
-  
+
     document.body.children[1].appendChild(scoreTag);
     document.getElementsByTagName("h1")[0].setAttribute("id", "score");
     document.getElementById("score").textContent = `Your score is: ${score}`;
     document.body.children[1].appendChild(inputTag);
     document.getElementsByTagName("input")[0].setAttribute("id", "input-field");
-  
+
     submitButton.textContent = "Submit";
     document.body.children[1].appendChild(submitButton);
     document.getElementsByTagName("button")[0].setAttribute("id", "btn");
-    submitButton.addEventListener("click", function (event) {
-      event.preventDefault();
-      let highScoreText = new Object();
-      highScoreText.name = inputTag.value.trim();
-      highScoreText.newScore = score;
-      storeScores(highScoreText);
-      window.location.href = "scores.html";
+    submitButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        let highScoreText = new Object();
+        highScoreText.name = inputTag.value.trim();
+        highScoreText.newScore = score;
+        storeScores(highScoreText);
+        window.location.href = "scores.html";
     });
-  }
+}
+// save to local storage 
 
-  
+function storeScores(highScoreText) {
+    tempArray = JSON.parse(localStorage.getItem("scores"));
+    if (tempArray === null) {
+        scoreList.push(highScoreText);
+        localStorage.setItem("scores", JSON.stringify(scoreList));
+    } else {
+        tempArray.push(highScoreText);
+        localStorage.setItem("scores", JSON.stringify(tempArray));
+    }
+}
 
+// hide buttons on page load
+answer1.hidden = true;
+answer2.hidden = true;
+answer3.hidden = true;
+answer4.hidden = true;
 
-
-
-
+// event listeners
+startButton.addEventListener("click", showQuestion);
+startButton.addEventListener("click", startTime);
+startButton.addEventListener("click", () => {
+    messageDiv.textContent = "";
+});
+answer1.addEventListener("click", choice0);
+answer2.addEventListener("click", choice1);
+answer3.addEventListener("click", choice2);
+answer4.addEventListener("click", choice3);
 
 
 // Questions Array
-questionIndex = [];
+const questions = [{
+        question: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<script>", "<js>", "<scripting>", "<javascript>"],
+        answer: "<script>"
+    },
 
-                var q1 = {
-                    text: "Inside which HTML element do we put the JavaScript?",
-                    choices: ["<script>", "<js>", "<scripting>", "<javascript>"],
-                    answer: "<script>"
-                };
-                questionIndex.push(q1);
+    {
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        choices: ["<script href='xxx.js'>", "<script ='xxx.js'>", "<script src='xxx.js'>", "<javascript ='xxx.js'>"],
+        answer: "<script src='xxx.js'>"
+    },
 
-                var q2 = {
-                    text: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-                    choices: ["<script href='xxx.js'>", "<script ='xxx.js'>", "<script src='xxx.js'>", "<javascript ='xxx.js'>"],
-                    answer: "<script src='xxx.js'>"
-                };
-                questionIndex.push(q2);
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        choices: ["msgBox('Hello World')", "alert('Hello World');", "alertBox('Hello World');", "msg('Hello World');"],
+        answer: "alert('Hello World');"
+    },
 
-                var q3 = {
-                    text: "How do you write 'Hello World' in an alert box?",
-                    choices: ["msgBox('Hello World')", "alert('Hello World');", "alertBox('Hello World');", "msg('Hello World');"],
-                    answer: "alert('Hello World');"
-                };
-                questionIndex.push(q3);
+    {
+        question: "How to write an IF statement in JavaScript?",
+        choices: ["if i = 5", "if (i = 5)", "if i = 5 then", "if i == 5 then"],
+        answer: "if (i = 5)"
+    },
 
-                var q4 = {
-                    text: "How to write an IF statement in JavaScript?",
-                    choices: ["if i = 5", "if (i = 5)", "if i = 5 then", "if i == 5 then"],
-                    answer: "if (i = 5)"
-                };
-                questionIndex.push(q4);
+    {
+        question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+        choices: ["if (i !=5)", "if i=!5 then", "if (i) !=5", "if i <> 5"],
+        answer: "if (i != 5)"
+    },
 
-                var q5 = {
-                    text: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
-                    choices: ["if (i !=5)", "if i=!5 then", "if (i) !=5", "if i <> 5"],
-                    answer: "if (i != 5)"
-                };
-                questionIndex.push(q5)
-            
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<script>", "<js>", "<scripting>", "<javascript>"],
+        answer: "<script>"
+    },
+
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        choices: ["onmouseclick", "onchange", "onclick", "onmouseover"],
+        answer: "onclick"
+    },
+
+    {
+        question: "How can you make a bulleted list in HTML?",
+        choices: ["<ol>", "<ul>", "<dl>", "<list>"],
+        answer: "<ul>"
+    },
+
+    {
+        question: "What does CSS stand for?",
+        choices: ["Colorful Style Sheets", "Creative Style Sheets", "Computer Style Sheets", "Cascading Style Sheets", ],
+        answer: "Cascading Style Sheets"
+    },
+
+    {
+        question: "What does HTML stand for?",
+        choices: ["Hyper Mark Text Language", "Hypermark Match Text Language", "Hypertext Markup Language", "Hypertext Match Language"],
+        answer: "Hypertext Markup Language"
+    },
+];
